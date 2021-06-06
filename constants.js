80b1f6d3e8677c62d1ec7ddc668937b2c6dbcb4b
LuckyDogeDev <84397643+LuckyDogeDev@users.noreply.github.com>
2021-06-06 10:13:27 -0400

    remove

--- constants.js
+++ constants.js
@@ -16,11 +16,11 @@ module.exports = {
         blocklytics: 'wss://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks'
     },
 
-    barAddress: "0x8798249c2e607446efb7ad49ec89dd1865ff4272",
-    makerAddress: "0xe11fc0b43ab98eb91e9836129d1ee7c3bc95df50",
-    minerAddress: "0xc2edad668740f1aa35e4d8f227fb8e17dca888cd",
-    goldnuggetAddress: "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2",
-    factoryAddress: "0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac",
+    barAddress: "",
+    makerAddress: "",
+    minerAddress: "",
+    goldnuggetAddress: "",
+    factoryAddress: "",
 
     TWENTY_FOUR_HOURS: 86400,
 }
