63141e3840ff7aa3b6523dd77de72ccecfa158c0
LuckyDogeDev <luckydogedev@protonmail.com>
2021-07-06 15:55:45 -0400

    [dev] Attach LIVE ETH addresses & File Rename

--- constants.js
+++ constants.js
@@ -16,11 +16,11 @@ module.exports = {
         blocklytics: 'wss://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks'
     },
 
-    alchemybenchAddress: "",
-    smelterAddress: "",
-    minerAddress: "",
-    goldnuggetAddress: "",
-    factoryAddress: "",
+    alchemybenchAddress: "0xCfbB59ba22F0B0dB768A7C00e8177bBB9b35B8B4",
+    smelterAddress: "0x59f84BbeE8b8cbE2aC45F1B7711f4034B2Ae0408",
+    minerAddress: "0xddB4E76521cA2DA81686Eb84106F15286BBe2Cdb",
+    goldnuggetAddress: "0xc6D69475f115F61B1e8C4e78c20C49201c869DB4",
+    factoryAddress: "0x58100D88DE401bf6be6a3E77D0Da0270648efbbE",
 
     TWENTY_FOUR_HOURS: 86400,
 }
